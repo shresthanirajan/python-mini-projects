4da2b87a3097a63107a5c7ccdf2ddd36b36840fc
CryptechPsycho <blu.niraj@gmail.com>
2026-08-18 21:34:33 -0500

    working on Project 6

---
+++ pratice-python/MiniInventoryTracker.py
@@ -0,0 +1,13 @@
+inventory = {
+    "apple": {"price": 1.50, "stock": 10},
+    "banana": {"price": 0.75, "stock": 20},
+    "orange": {"price": 1.25, "stock": 8}
+}
+
+def find_product(product_name):
+    if product_name in inventory:
+        return 
+
+for product, info in inventory.items():
+    print(f"{product} - Price: {info['price']} - Stock: {info['stock']}")
+
