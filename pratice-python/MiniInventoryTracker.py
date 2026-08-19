2622c82c0a21f2b14b3e2c26fdba9b81bd10db1b
CryptechPsycho <blu.niraj@gmail.com>
2026-08-19 17:28:15 -0500

    Working On Mini-Project 6

--- pratice-python/MiniInventoryTracker.py
+++ pratice-python/MiniInventoryTracker.py
@@ -6,8 +6,20 @@ inventory = {
 
 def find_product(product_name):
     if product_name in inventory:
-        return
+        return inventory[product_name]
+    else:
+        return None
+
+
 
 for product, info in inventory.items():
     print(f"{product} - Price: {info['price']} - Stock: {info['stock']}")
+
+product_name = input("Enter product name: ").lower()
+product = find_product(product_name)
+if product == "Product not found":
+    print("Product Not here")
+else:
+    print(f"Price: {product['price']}\nStock: {product['stock']}")
+
 
