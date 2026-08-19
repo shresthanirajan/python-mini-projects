9246a5f71393364e3aa28a719d3f62502aedbdd7
CryptechPsycho <blu.niraj@gmail.com>
2026-08-19 17:46:29 -0500

    Implemented sell_product Function

--- pratice-python/MiniInventoryTracker.py
+++ pratice-python/MiniInventoryTracker.py
@@ -1,15 +1,17 @@
+#Current Inverntory
 inventory = {
     "apple": {"price": 1.50, "stock": 10},
     "banana": {"price": 0.75, "stock": 20},
     "orange": {"price": 1.25, "stock": 8}
 }
 
+#Find Product
 def find_product(product_name):
     if product_name in inventory:
         return inventory[product_name]
     else:
         return None
-
+#Update Function
 def update_stock(product_name, amount):
     if product_name in inventory:
         if amount > 0:
@@ -19,9 +21,26 @@ def update_stock(product_name, amount):
             print("Please enter a valid amount number.")
     else:
         print("Product doesn't exists")
+
+def sell_product(product_name, amount):
+    if product_name in inventory:
+        if amount > 0:
+            if inventory[product_name]["stock"] >= amount:
+                inventory[product_name]["stock"] -= amount
+                print("DONE")
+            else:
+                print("Please enter valid Amount to sell.")
+        else:
+            print("Please enter valid amount number.")
+    else:
+        print("Product doesn't exists")
+
+
+#Prints inventory Values
 for product, info in inventory.items():
     print(f"{product} - Price: {info['price']} - Stock: {info['stock']}")
 
+#Finding Products
 product_name = input("Enter product name: ").lower()
 product = find_product(product_name)
 if product == "Product not found":
@@ -30,5 +49,5 @@ else:
     print(f"Price: {product['price']}\nStock: {product['stock']}")
 
 
-update_stock("apple", 10)
+sell_product("apple", 100)
 print(inventory["apple"])
