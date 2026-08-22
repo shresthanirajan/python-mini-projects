9f4248e2912d41bc5062cb47304bad0ef5f4c072
CryptechPsycho <blu.niraj@gmail.com>
2026-08-22 03:30:56 -0500

    Project 8 Completed

--- pratice-python/TaskListManager.py
+++ pratice-python/TaskListManager.py
@@ -1,6 +1,6 @@
 tasks = [
     {"task": "study python", "done": True},
-    {"task": "finish homework", "done": True},
+    {"task": "finish homework", "done": False},
     {"task": "go to gym", "done": True}
 ]
 #Shows All the Tasks
@@ -23,8 +23,11 @@ def uncomplete_task(task_number):
   tasks[index]["done"] = False
 
 #Adds Task
-def add_task(task):
-  tasks.append(task)
+def add_task(task_name):
+  tasks.append({
+    "task": task_name,
+    "done": False
+  })
 
 #Removes a task
 def remove_task(task_number):
@@ -34,22 +37,30 @@ def remove_task(task_number):
 #Update Task
 def update_task(task_number, new_task):
   index = task_number -1
-  tasks[index] = new_task
+  tasks[index]["task"] = new_task
 
 #Search Function
 def search_tasks(search_term):
   found = False
   for task in tasks:
-    if search_term.lower() in task.lower():
+    if search_term.lower() in task["task"].lower():
       print(f"{task} is here!")
       found = True
   if not found:
     print("Not here")
 
-def complete_task(task_number):
-  pass
+#Show completed Task
+def show_completed_tasks():
+  for task in tasks:
+    if task["done"]:
+      print(task["task"])
 
-show_tasks()
+#Show Incompleted Tasks
+def show_incomplete_tasks():
+  for task in tasks:
+    if not task["done"]:
+      print(task["task"])
+
 
 completed = 0
 not_completed = 0
@@ -63,6 +74,15 @@ for task in tasks:
   else:
     not_completed += 1
 
-print(f"Task completed: {completed}")
-print(task_completed)
-print(f"Task Not completed: {not_completed}")
+# print(f"Task completed: {completed}")
+# print(task_completed)
+# print(f"Task Not completed: {not_completed}")
+
+
+# show_completed_tasks()
+
+# show_incomplete_tasks()
+
+
+remove_task(2)
+print(tasks)
