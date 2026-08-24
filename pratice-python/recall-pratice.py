53fb8751fdca37a28f519c5bcdc5e988c1af8399
CryptechPsycho <blu.niraj@gmail.com>
2026-08-23 23:27:55 -0500

    Fixing Issues in warm-up

--- pratice-python/recall-pratice.py
+++ pratice-python/recall-pratice.py
@@ -32,5 +32,45 @@ contacts = {
     "maya": "222"
 }
 
-contacts = ["sam"] = "333"
-contacts["alex"] = 999
+contacts["sam"] =   "333"
+contacts["alex"] = "999"
+
+print(contacts)
+
+#Contacts .items gives you the key and value of the contacts so name and number
+#Contacts .values gives you the value it self so it will be numbers only 
+#Contacts .Keys gives you the key so the name 
+
+inventory = {
+    "apple": {"price": 1.50, "stock": 10},
+    "banana": {"price": 0.75, "stock": 4},
+    "orange": {"price": 1.25, "stock": 8}
+}
+
+lowest_item = inventory["apple"]["stock"]
+
+for name ,value in inventory.items():
+  if value["stock"] < lowest_item:
+    lowest_item = value["stock"]
+    lowest_name = name
+print(lowest_item)
+print(lowest_name)
+  
+
+expenses = [
+    {"name": "food", "amount": 25},
+    {"name": "gas", "amount": 40},
+    {"name": "coffee", "amount": 6}
+]
+
+highest_amount = expenses[0]["amount"]
+lowest_amount = expenses[0]["amount"]
+for item in expenses:
+  if item["amount"] > highest_amount:
+    highest_amount = item["amount"]
+    highest_expense_name = item["name"]
+  if item ["amount"] < lowest_amount:
+    lowest_amount = item["amount"]
+    lowest_expense_name = item["name"]
+print(highest_expense_name, highest_amount)
+print(lowest_expense_name, lowest_amount)
