1698a11f24e40aa8b774c536b148e0dad3041c46
CryptechPsycho <blu.niraj@gmail.com>
2026-08-25 01:13:20 -0500

    Warm up almost completed

--- pratice-python/recall-pratice.py
+++ pratice-python/recall-pratice.py
@@ -59,5 +59,86 @@ def remove_expense(expense_number):
     expenses.pop(expense_number-1)
     return
 
-remove_expense(1)
-print(expenses)
+def expenses_in_range(min_amount, max_amount):
+  for expense in expenses:
+    if min_amount >= 0 or max_amount <= 0:
+      if min_amount > max_amount:
+        print("Invalid range")
+        return
+      else:
+        if min_amount <= expense["amount"] <= max_amount:
+          print(expense)
+    else:
+      print("Amounts cannot be negative")
+      return
+
+# remove_expense(2)
+# print(expenses)
+expenses_in_range(7,25)
+
+
+print(" ")
+inventory = {
+    "keyboard": {"price": 70, "stock": 12},
+    "mouse": {"price": 30, "stock": 4},
+    "monitor": {"price": 220, "stock": 7},
+    "headphones": {"price": 90, "stock": 2}
+}
+
+lowest_stock = inventory["keyboard"]["price"]
+lowest_stock_name = ''
+
+for name, item in inventory.items():
+  if item["stock"] < lowest_stock:
+    lowest_stock = item["stock"]
+    lowest_stock_name = name
+
+
+print(f"Lowest stock product: {lowest_stock_name}")
+print(f"Stock: {lowest_stock}")
+
+
+expenses = [
+    {"name": "rent", "amount": 900},
+    {"name": "food", "amount": 200},
+    {"name": "gas", "amount": 100}
+]
+
+higest_amount = expenses[0]["amount"]
+higest_expense = expenses[0]["name"]
+
+
+for amount in expenses:
+  if higest_amount < amount["amount"]:
+    higest_amount = amount["amount"]
+    higest_expense = amount["name"]
+    
+    
+                      
+
+print(higest_amount) 
+print(higest_expense)
+
+#9 Should Print nothing 
+
+expenses = [
+    {"name": "food", "amount": 25},
+    {"name": "gas", "amount": 40},
+    {"name": "coffee", "amount": 6},
+    {"name": "books", "amount": 40}
+]
+
+def find_expenses_over(limit):
+  if limit > 0:
+    found = False
+    for expense in expenses:
+      if expense["amount"] > limit:
+        print(expense["amount"])
+        found = True
+      if not found:
+        print("No expenses Found")
+        return
+  else:
+    print("limit cannot be negative")
+
+find_expenses_over(2)
