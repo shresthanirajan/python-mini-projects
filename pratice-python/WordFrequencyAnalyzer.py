3f889f9f659198a803afd285d80950db3fad1b86
CryptechPsycho <blu.niraj@gmail.com>
2026-08-25 20:19:03 -0500

    Finished Project 11 WordFrequencyAnalyzer

--- pratice-python/WordFrequencyAnalyzer.py
+++ pratice-python/WordFrequencyAnalyzer.py
@@ -1,10 +1,15 @@
 sentence = "Python is fun, and python is powerful!"
 
 def count_words(sentence):
+  if not sentence.strip():
+      print("Please Enter a Sentence")
+      return
   words = sentence.lower().split()
   word_counts = {}
   highest_count = 0
   highest_word = ""
+  repeated_words = [] 
+  unique_words = []
   
   for word in words:
     word = word.strip(",.!")
@@ -18,8 +23,14 @@ def count_words(sentence):
       highest_count = count
       highest_word = word
     if count > 1:
-      print(word)
-  return word_counts, highest_count, highest_word 
-word_counts, highest_count, highest_word  = count_words(sentence)
-print(word_counts)
-print(f"Highest Word: {highest_word} | Highest count: {highest_count}")
+      repeated_words.append(word)
+    else:
+      unique_words.append(word)
+  return word_counts, highest_count, highest_word, repeated_words, unique_words
+
+result = count_words(sentence)
+if result:
+    word_counts, highest_count, highest_word, repeated_words, unique_words = result
+    print(f"Highest Word: {highest_word} | Highest count: {highest_count}")
+    print(f"Repeated words: {repeated_words}")
+    print(f"Unique words: {unique_words}")
