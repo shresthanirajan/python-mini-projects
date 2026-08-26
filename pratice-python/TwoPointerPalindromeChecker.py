a2590929bf7852c8c88b2109070d9c26929c58f8
CryptechPsycho <blu.niraj@gmail.com>
2026-08-26 18:21:00 -0500

    TwoPointerPalindromeChecker

--- pratice-python/TwoPointerPalindromeChecker.py
+++ pratice-python/TwoPointerPalindromeChecker.py
@@ -12,3 +12,31 @@ def is_palindrome(text):
       return False
   return True
 is_palindrome("racecar")
+
+text = "abcdefg"
+
+left = 0 #0
+right = len(text) -1 #6
+
+while left < right:
+  print(text[left], text[right])
+  
+  left += 1
+  right -=1
+
+text = "level"
+
+left = 0
+right = len(text)-1
+is_palindrome = True
+while left < right:
+  if text[left] == text[right]:
+    print(text[left], text[right], "True")
+    
+    left += 1
+    right -= 1
+  else:
+    is_palindrome = False 
+    break
+print(is_palindrome)
+
