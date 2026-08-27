39be3a6e2da7a6b0a248ee4dd23dcb1900d5c13f
CryptechPsycho <blu.niraj@gmail.com>
2026-08-27 17:55:16 -0500

    Testing Cases

--- pratice-python/recall-pratice.py
+++ pratice-python/recall-pratice.py
@@ -1,9 +1,28 @@
-#1
-text = "banana"
-text_dic = {}
-for char in text:
-  if char in text_dic:
-    text_dic[char] += 1
-  else:
-    text_dic[char] = 1
-print(text_dic)
+# #1
+# text = "banana"
+# text_dic = {}
+# for char in text:
+#   if char in text_dic:
+#     text_dic[char] += 1
+#   else:
+#     text_dic[char] = 1
+# print(text_dic)
+
+#2
+def unique_character():
+  text = "aabbcddee"
+  text_dic = {}
+  for char in text:
+    if char in text_dic:
+     text_dic[char] += 1
+    else:
+      text_dic[char] = 1
+  for char in text_dic.values():
+    if char <= 1:
+      return char
+  
+
+test = unique_character()
+print(test)
+
+
