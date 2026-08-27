dc46fa75b0e4c9e3a3d7e97ebc3878cc0ccdcd77
CryptechPsycho <blu.niraj@gmail.com>
2026-08-27 17:41:12 -0500

    learning

--- pratice-python/FirstNonRepeatingCharacter.py
+++ pratice-python/FirstNonRepeatingCharacter.py
@@ -1,18 +1,23 @@
-def first_unique_char(text):
-  text = text.lower()
-  counts = {}
-  for char in text:
-    if char in counts:
-      counts[char] += 1
-    else:
-      counts[char] = 1
+# def first_unique_char(text):
+#   text = text.lower()
+#   counts = {}
+#   for char in text:
+#     if char in counts:
+#       counts[char] += 1
+#     else:
+#       counts[char] = 1
 
-  for i, char in enumerate(text):
-    if counts[char] == 1:
-      return i
+#   for i, char in enumerate(text):
+#     if counts[char] == 1:
+#       return i
   
-  return None
+#   return None
   
 
-result = first_unique_char("aabbccd")
-print(result)
+# result = first_unique_char("aabbccd")
+# print(result)
+
+for x in (1,2,3):
+  for y in (1,2):
+    print(x,y)
+  
