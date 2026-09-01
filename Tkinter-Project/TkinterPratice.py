3648f2d5fcdbc5c30fa21a875dacc7039f53708b
CryptechPsycho <blu.niraj@gmail.com>
2026-08-31 22:41:25 -0500

    Changed Settings

--- Tkinter-Project/TkinterPratice.py
+++ Tkinter-Project/TkinterPratice.py
@@ -7,6 +7,14 @@ label.pack()
 
 window.mainloop()
 
+from tkinter import *
+
+window = Tk()
+
+label = Label(window, text="Hello World", font=('Arial', 40,'bold'),fg='blue')
+label.pack()
+
+window.mainloop()
 
 
 
