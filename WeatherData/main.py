60d88a40d661d26d60388a4692ab68d8314bf217
CryptechPsycho <blu.niraj@gmail.com>
2026-09-01 16:53:08 -0500

    Finished Short Pandas Squirrel Project

--- WeatherData/main.py
+++ WeatherData/main.py
@@ -1,26 +1,53 @@
+# import pandas
+
+# data = pandas.read_csv("weather_data.csv")
+
+# # data_dict = data.to_dict()
+# # print(data_dict)
+
+# # temp_list = data["temp"].to_list()
+
+
+# # print(data["temp"].mean())
+# # print(data["temp"].max())
+
+# # #Get Data in Columns
+# # print(data["condition"])
+# # print(data.condition)
+
+# #Get Data in Row
+# # print(data[data.day == "Tuesday"])
+# # print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == "Monday"]
+# monday_temperture = ((monday.temp * 1.8) + 32)
+# print(monday_temperture)
+
+
+# #Create a dataframe From scratch
+# data_dict = {
+#   "students": ["Amy", "James", "Angela"],
+#   "scores": [76, 56, 65]
+# }
+
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
+
 import pandas
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20260901.csv")
 
-data = pandas.read_csv("weather_data.csv")
-
-# data_dict = data.to_dict()
-# print(data_dict)
-
-# temp_list = data["temp"].to_list()
-
-
-# print(data["temp"].mean())
-# print(data["temp"].max())
-
-# #Get Data in Columns
-# print(data["condition"])
-# print(data.condition)
-
-#Get Data in Row
-# print(data[data.day == "Tuesday"])
-# print(data[data.temp == data.temp.max()])
-
-monday = data[data.day == "Monday"]
+gray_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+print(gray_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
 
 
-monday_temperture = ((monday.temp * 1.8) + 32)
-print(monday_temperture)
+data_dict = {
+  "Fur Color": ["Gray", "Cinnamon", "Black"],
+  "Count": [gray_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
