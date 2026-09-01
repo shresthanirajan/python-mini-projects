0b233398d9d41a06c4c2cf929e4fd4fc952e8bd9
CryptechPsycho <blu.niraj@gmail.com>
2026-08-31 22:42:07 -0500

    Calulator File

--- Tkinter-Project/TkinterPratice.py
+++ Tkinter-Project/TkinterPratice.py
@@ -15,6 +15,23 @@ label = Label(window, text="Hello World", font=('Arial', 40,'bold'),fg='blue')
 label.pack()
 
 window.mainloop()
+from tkinter import *
+
+window = Tk()
+
+label = Label(window, text="Hello World", font=('Arial', 40,'bold'),fg='blue')
+label.pack()
+
+window.mainloop()
+
+from tkinter import *
+
+window = Tk()
+
+label = Label(window, text="Hello World", font=('Arial', 40,'bold'),fg='blue')
+label.pack()
+
+window.mainloop()
 
 
 
