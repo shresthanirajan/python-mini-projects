7a757dc791ce486fc7656e30d8a7b11adef4a4fd
CryptechPsycho <blu.niraj@gmail.com>
2026-09-02 17:01:00 -0500

    Working on small projects with Pandas

---
+++ LearningPandas/AIModeResultsTracker.py
@@ -0,0 +1,37 @@
+import pandas as pd
+data = {"Model": ["Logistic Regession", "Random Forest", "Decision Tree", "KNN"], "accuracy": [0.84, 0.91, 0.79, 0.86],
+        "training_time": [12, 35, 8, 15]}
+
+data_set = pd.DataFrame(data)
+
+# print(data_set[["Model", "accuracy"]])
+
+# print(data_set[ 
+#   data_set["accuracy"] >= 0.85
+# ])
+
+# print(data_set[
+#   data_set["training_time"] < 20
+# ])
+
+# print(data_set[
+#   (data_set["training_time"] < 20) & (data_set["accuracy"] > 0.80)
+# ])
+
+# print(data_set[
+#   (data_set["accuracy"] > 0.90) | (data_set["training_time"] < 10)
+# ])
+
+# print(data_set[
+#   data_set["accuracy"] > 0.85 & data_set["Model"]
+# ])
+
+print(data_set[
+  data_set["accuracy"] > 0.85
+][["Model", "accuracy"]])
+
+print(data_set[
+  data_set["training_time"] < 20]
+  [["Model", "training_time"]]
+)
+
