c761a3ba522d9048ef3a93c1555454492891bb85
CryptechPsycho <blu.niraj@gmail.com>
2026-09-03 16:16:08 -0500

    learning and adding in pandas

--- LearningPandas/AIModeResultsTracker.py
+++ LearningPandas/AIModeResultsTracker.py
@@ -4,61 +4,84 @@ data = {"Model": ["Logistic Regession", "Random Forest", "Decision Tree", "KNN"]
 
 data_set = pd.DataFrame(data)
 
+# print(data_set)
+
+# print(data_set["accuracy"])
+
 # print(data_set[["Model", "accuracy"]])
 
-# print(data_set[ 
-#   data_set["accuracy"] >= 0.85
+# print(data_set["accuracy"] > 0.85)
+
+# print(data_set["training_time"] < 20)
+
+# print(data_set[
+#   data_set["training_time"] < 20]
+#   [["Model", "accuracy"]]
+# )
+
+# print(data_set[
+#   (data_set["accuracy"] > 0.85) & (data_set["training_time"] < 20)
+# ])
+
+# print(data_set[
+#   data_set["accuracy"] > 0.85
+# ]
+# [
+
+#   ["Model", "accuracy"]
+# ]
+# )
+
+# #1
+# print(data_set[
+#   data_set["training_time"] <= 15
+# ]
+
+#   [
+#     ["Model"]
+#   ]
+# )
+
+# #2
+# print(data_set[
+#   data_set["accuracy"] < 0.90
+# ]
+# [
+#   ["Model", "training_time"]
+# ]
+# )
+# #3 
+# print(data_set[
+#   (data_set["accuracy"] >= 0.80) & (data_set["training_time"] < 15)
+# ]
+# [["Model"]]
+# )
+
+# #4
+# print(data_set[
+#   (data_set["accuracy"] > 0.90) | (data_set["training_time"] <= 8)
+# ]
+# [["Model"]])
+
+# #5
+# print(data_set[
+#   data_set["training_time"] > 10 
+# ]
+# [["Model"]])
+
+# print(data_set[
+#   (data_set["accuracy"] >= 0.80) & (data_set["accuracy"] <= 0.90)
 # ])
 
 # print(data_set[
 #   data_set["training_time"] < 20
-# ])
+
+# ]
+# [["Model", "accuracy"]])
 
 # print(data_set[
-#   (data_set["training_time"] < 20) & (data_set["accuracy"] > 0.80)
-# ])
+#   (data_set["accuracy"] >= 0.8) & (data_set["training_time"] <= 15)
+# ]
+# [["Model"]])
 
-# print(data_set[
-#   (data_set["accuracy"] > 0.90) | (data_set["training_time"] < 10)
-# ])
-
-# print(data_set[
-#   data_set["accuracy"] > 0.85 & data_set["Model"]
-# ])
-
-print(data_set[
-  data_set["accuracy"] > 0.85
-][["Model", "accuracy"]])
-
-print(data_set[
-  data_set["training_time"] < 20]
-  [["Model", "training_time"]]
-)
-print(data_set
-  [["Model", "training_time"]]
-)
-print(data_set
-  [["training_time", "training_time"]]
-)
-
-#TODO adding featues into data_set
-print(data_set[
-  data_set["training_time"] < 20]
-  [["Model", "training_time"]]
-)
-print(data_set
-  [["Model", "training_time"]]
-)
-print(data_set
-  [["training_time", "training_time"]]
-)
-print(data_set[
-  data_set["training_time"] < 20]
-  [["Model", "training_time"]]
-)
-print(data_set
-  [["Model", "training_time"]]
-)
-print(data_set
-  [["training_time", "training_time"]]
-)
+#Adding and Updating
