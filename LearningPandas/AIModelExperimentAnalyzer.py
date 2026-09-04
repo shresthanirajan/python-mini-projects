0618e1bd60780c7bf4317831aacb352d8d2e1535
CryptechPsycho <blu.niraj@gmail.com>
2026-09-04 04:13:16 -0500

    Finished Cleaning up Messy CSV Project 2

--- LearningPandas/AIModelExperimentAnalyzer.py
+++ LearningPandas/AIModelExperimentAnalyzer.py
@@ -7,23 +7,23 @@ print(df)
 #3
 print(df.head())
 
-#4
+# #4
 print(df[["model", "accuracy"]])
 
-#5
-print(df["accuracy"].mean())
-#6
-print(df["accuracy"].max())
-#7
-print(df[df["accuracy"] > 0.85]
-      [["model"]])
-#8
-print(df[df["training_time"] < 30]
-      ["model"])
-#9
-print(df.sort_values(by="accuracy", ascending=False))
+# #5
+# print(df["accuracy"].mean())
+# #6
+# print(df["accuracy"].max())
+# #7
+# print(df[df["accuracy"] > 0.85]
+#       [["model"]])
+# #8
+# print(df[df["training_time"] < 30]
+#       ["model"])
+# #9
+# print(df.sort_values(by="accuracy", ascending=False))
 
-#10
-df["accuracy_percent"] = df["accuracy"] * 100
-
+# #10
+# df["accuracy_percent"] = df["accuracy"] * 100
+print(df["accuracy"] > 0.70)
 
--- LearningPandas/messyCSVcleaning.py
+++ LearningPandas/messyCSVcleaning.py
@@ -1,3 +1,30 @@
 import pandas as pd 
+#1
+df = pd.read_csv("customer_data.csv")
+#2
+print(df.head())
+#3
+print(df.isna().sum())
+#4
+print(df[
+  df["age"].isna()
+])
+#5
+print(df[
+  df["income"].isna()
+])
+#6
+df["age"] = df["age"].fillna(df["age"].mean())
+#7
+df["income"] = df["income"].fillna(df["income"].mean())
 
-df = pd.DataFrame()
+#8 Cleaned DataFrame
+print(df)
+
+#9 - Average Income After Cleaning
+print(df["income"].mean())
+
+#10
+print(df[
+  df["city"] == "Austin"
+])
