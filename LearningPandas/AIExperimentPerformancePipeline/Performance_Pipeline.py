b99f66184381382e8bdfd8d32762ebf57adb420a
CryptechPsycho <blu.niraj@gmail.com>
2026-09-05 22:35:57 -0500

    Working on pandas Proejct

---
+++ LearningPandas/AIExperimentPerformancePipeline/Performance_Pipeline.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df_models = pd.read_csv("AIExperimentPerformancePipeline/models.csv")
+df_results = pd.read_csv("AIExperimentPerformancePipeline/experiment_results.csv")
+# print(df_models)
+print(df_models)
+
+print(df_results)
+
+print(df_results.duplicated().sum())
