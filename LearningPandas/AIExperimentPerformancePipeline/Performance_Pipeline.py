530fe8d10b4a50ef01af51ec83e7569683353291
CryptechPsycho <blu.niraj@gmail.com>
2026-09-05 22:36:34 -0500

    Fixed the errors

--- LearningPandas/AIExperimentPerformancePipeline/Performance_Pipeline.py
+++ LearningPandas/AIExperimentPerformancePipeline/Performance_Pipeline.py
@@ -11,3 +11,7 @@ print(df_models)
 print(df_results)
 
 print(df_results.duplicated().sum())
+
+df_models = pd.read_csv("AIExperimentPerformancePipeline/models.csv")
+df_results = pd.read_csv("AIExperimentPerformancePipeline/experiment_results.csv")
+
