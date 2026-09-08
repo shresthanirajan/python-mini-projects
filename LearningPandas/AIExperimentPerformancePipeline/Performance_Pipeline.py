f7948d9c7ceac92159ee92661440cae67b26914e
CryptechPsycho <blu.niraj@gmail.com>
2026-09-07 21:43:27 -0500

    Working on pj

--- LearningPandas/AIExperimentPerformancePipeline/Performance_Pipeline.py
+++ LearningPandas/AIExperimentPerformancePipeline/Performance_Pipeline.py
@@ -2,10 +2,9 @@ import pandas as pd
 
 df_models = pd.read_csv("AIExperimentPerformancePipeline/models.csv")
 df_results = pd.read_csv("AIExperimentPerformancePipeline/experiment_results.csv")
-# print(df_models)
 
 
-
+print(df_models.head())
 
 
 
