2bcfa83d5cf9a753df233d3bbdd6cdd23a46e476
CryptechPsycho <blu.niraj@gmail.com>
2026-09-07 21:44:25 -0500

    finsished warmup

--- LearningPandas/AIExperimentPerformancePipeline/Performance_Pipeline.py
+++ LearningPandas/AIExperimentPerformancePipeline/Performance_Pipeline.py
@@ -4,18 +4,6 @@ df_models = pd.read_csv("AIExperimentPerformancePipeline/models.csv")
 df_results = pd.read_csv("AIExperimentPerformancePipeline/experiment_results.csv")
 
 
-print(df_models.head())
-
-
-
-
-df_models = pd.read_csv("AIExperimentPerformancePipeline/models.csv")
-
-
-
-print(df_models.head())
-
-
 
 
 
