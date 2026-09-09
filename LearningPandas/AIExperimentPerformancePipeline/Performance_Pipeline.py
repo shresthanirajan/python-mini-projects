f632824a6966805b7ab6328e8326aa647822b782
CryptechPsycho <blu.niraj@gmail.com>
2026-09-08 23:55:41 -0500

    Trying out new stuff

--- LearningPandas/AIExperimentPerformancePipeline/Performance_Pipeline.py
+++ LearningPandas/AIExperimentPerformancePipeline/Performance_Pipeline.py
@@ -8,3 +8,7 @@ df_results = pd.read_csv("AIExperimentPerformancePipeline/experiment_results.csv
 
 df_models = pd.read_csv("AIExperimentPerformancePipeline/models.csv")
 df_results = pd.read_csv("AIExperimentPerformancePipeline/experiment_results.csv")
+
+
+df_models = pd.read_csv("AIExperimentPerformancePipeline/models.csv")
+df_results = pd.read_csv("AIExperimentPerformancePipeline/experiment_results.csv")
