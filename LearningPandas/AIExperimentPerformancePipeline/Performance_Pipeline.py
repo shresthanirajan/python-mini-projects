0f42d712a3d45a5897520f3d2444df6e85450722
CryptechPsycho <blu.niraj@gmail.com>
2026-09-08 23:56:58 -0500

    df_result changed

--- LearningPandas/AIExperimentPerformancePipeline/Performance_Pipeline.py
+++ LearningPandas/AIExperimentPerformancePipeline/Performance_Pipeline.py
@@ -4,17 +4,3 @@ df_models = pd.read_csv("AIExperimentPerformancePipeline/models.csv")
 df_results = pd.read_csv("AIExperimentPerformancePipeline/experiment_results.csv")
 
 
-
-
-df_models = pd.read_csv("AIExperimentPerformancePipeline/models.csv")
-df_results = pd.read_csv("AIExperimentPerformancePipeline/experiment_results.csv")
-
-
-df_models = pd.read_csv("AIExperimentPerformancePipeline/models.csv")
-df_results = pd.read_csv("AIExperimentPerformancePipeline/experiment_results.csv")
-
-df_models = pd.read_csv("AIExperimentPerformancePipeline/models.csv")
-df_results = pd.read_csv("AIExperimentPerformancePipeline/experiment_results.csv")
-
-df_models = pd.read_csv("AIExperimentPerformancePipeline/models.csv")
-df_results = pd.read_csv("AIExperimentPerformancePipeline/experiment_results.csv")
