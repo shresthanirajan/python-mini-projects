244cf1c4345d4072053cab7bf4f6ed16f26f91bc
CryptechPsycho <blu.niraj@gmail.com>
2026-09-08 23:56:10 -0500

    Pandas Testing

--- LearningPandas/AIExperimentPerformancePipeline/Performance_Pipeline.py
+++ LearningPandas/AIExperimentPerformancePipeline/Performance_Pipeline.py
@@ -12,3 +12,6 @@ df_results = pd.read_csv("AIExperimentPerformancePipeline/experiment_results.csv
 
 df_models = pd.read_csv("AIExperimentPerformancePipeline/models.csv")
 df_results = pd.read_csv("AIExperimentPerformancePipeline/experiment_results.csv")
+
+df_models = pd.read_csv("AIExperimentPerformancePipeline/models.csv")
+df_results = pd.read_csv("AIExperimentPerformancePipeline/experiment_results.csv")
