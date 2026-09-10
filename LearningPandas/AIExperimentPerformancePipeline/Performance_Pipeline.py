8251be793f37b17d7b213f4843097b14ceccf4ca
CryptechPsycho <blu.niraj@gmail.com>
2026-09-09 23:17:36 -0500

    Working on the project

--- LearningPandas/AIExperimentPerformancePipeline/Performance_Pipeline.py
+++ LearningPandas/AIExperimentPerformancePipeline/Performance_Pipeline.py
@@ -1,6 +1,21 @@
 import pandas as pd
 
+#1
 df_models = pd.read_csv("AIExperimentPerformancePipeline/models.csv")
 df_results = pd.read_csv("AIExperimentPerformancePipeline/experiment_results.csv")
 
+#2
+print(df_models.head())
+print(df_results.head())
 
+#3
+print(df_results.duplicated().sum())
+
+#4
+df_results = df_results.drop_duplicates()
+print(df_results)
+
+#5
+df_results["accuracy"] = df_results["accuracy"].fillna(
+  df_results["accuracy"].mean()
+)
