7bb387828808cc0f40454a0c79f2642529ddcd39
CryptechPsycho <blu.niraj@gmail.com>
2026-09-21 21:52:13 -0500

    reset

--- LearningPandas/04_groupby/ModelResultsbyDataset.py
+++ LearningPandas/04_groupby/ModelResultsbyDataset.py
@@ -33,19 +33,6 @@ print(df[
 ]["accuracy"].mean())
 
 
-print(df[
-  df["training_time"] < 30
-]["accuracy"].mean())
-
-
-print(df[
-  df["training_time"] < 30
-]["accuracy"].mean())
-
-
-print(df[
-  df["training_time"] < 30
-]["accuracy"].mean())
 
 
 
